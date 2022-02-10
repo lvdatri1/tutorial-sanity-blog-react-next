3846de4483708827d5f5519f08018fd32c404585
Trinh Le <lvdatri@gmail.com>
2022-02-10 14:48:45 +1300

    updated

--- web/pages/index.js
+++ web/pages/index.js
@@ -3,23 +3,25 @@ import groq from 'groq'
 import client from '../client'
 
 function Index(props) {
-    const { posts = [] } = props
-    return (
-      <div>
-        <h1>Welcome to a blog!</h1>
-        {posts.map(
-          ({ _id, title = "", slug = "", _updatedAt = "" }) =>
-            slug && (
-              <li key={_id}>
-                <Link href="/post/[slug]" as={`/post/${slug.current}`}>
-                  <a>{title}</a>
-                </Link>{" "}
-                ({new Date(_updatedAt).toDateString()})
-              </li>
-            )
-        )}
-      </div>
-    );
+  const { posts = [] } = props
+  return (
+    <div>
+      <h1>Welcome to a blog! with some pre defined set up </h1>
+      <div>1. Youtube content</div>
+      <div>2. video file</div>
+      {posts.map(
+        ({ _id, title = "", slug = "", _updatedAt = "" }) =>
+          slug && (
+            <li key={_id}>
+              <Link href="/post/[slug]" as={`/post/${slug.current}`}>
+                <a>{title}</a>
+              </Link>{" "}
+              ({new Date(_updatedAt).toDateString()})
+            </li>
+          )
+      )}
+    </div>
+  );
 }
 
 Index.getInitialProps = async () => ({
